4e431d5146f936b5f3b63008026367a9a5e197df
kwesi-arthur <bismarka531@gmail.com>
2023-06-21 23:15:51 -0700

    a program that finds and prints the sum of the even-valued terms, followed by a new line.
    You are allowed to use the standard library

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- *main - prints the sum of even fibonacci numbers
- *less than 4,000,000
+ * main - Prints the sum of even Fibonacci numbers
  *
- *@i, @j, @k, @sum- the number
- *Return : Always 0 (success)
+ * less than 4000000
+ *
+ * Return: Always 0 (success)
  */
 
 int main(void)
+
 {
 int i = 0;
 long j = 1, k = 2, sum = k;
@@ -17,11 +18,12 @@ while (k + j < 4000000)
 {
 k += j;
 if (k % 2 == 0)
+{
 sum += k;
 j = k - j;
 ++i;
 }
+}
 printf("%ld\n", sum);
-
 return (0);
 }
